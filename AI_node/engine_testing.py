0b7a60dd1a2be4577582fe93303910e60f75bba1
x8w8x <48895411+x8w8x@users.noreply.github.com>
2020-03-11 13:59:18 -0700

    Update engine_testing.py

--- AI_node/engine_testing.py
+++ AI_node/engine_testing.py
@@ -1,11 +1,11 @@
 import chess
 import chess.engine
 
-engine_stock = chess.engine.SimpleEngine.popen_uci("/home/mingyu/Documents/stockfish-11-linux/Linux/stockfish_20011801_x64")
+engine_stock = chess.engine.SimpleEngine.popen_uci("~/Documents/stockfish-11-linux/Linux/stockfish_20011801_x64")
 extreme_setup = {'Threads': 10, 'Hash': 5000, 'Skill Level': 20,}
 engine_stock.configure(extreme_setup)
 
-engine_komodo = chess.engine.SimpleEngine.popen_uci("/home/mingyu/Documents/komodo-10_ae4bdf/Linux/komodo-10-linux")
+engine_komodo = chess.engine.SimpleEngine.popen_uci("~/Documents/komodo-10_ae4bdf/Linux/komodo-10-linux")
 
 board = chess.Board()
 total_action = 0
